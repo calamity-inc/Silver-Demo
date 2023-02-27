6d3672b528239e99563f12e5a6848e94616df929
Sainan <63328889+Sainan@users.noreply.github.com>
2023-02-27 14:16:04 +0100

    Add .slv-loader to utility page

--- utility.php
+++ utility.php
@@ -5,6 +5,10 @@
 	<?php require "components/navbar.html"; ?>
 	<div>
 		<h1>Utility</h1>
+		<p>Miscellaneous useful classes provided by Silver.</p>
+		<h2>.slv-hidden</h2>
 		<p>There is the .slv-hidden utility class, but it's kinda hard to <i>show</i>.<span class="slv-hidden"> See what I mean?</span></p>
+		<h2>.slv-loader</h2>
+		<span class="slv-loader"></span>
 	</div>
 </body>
