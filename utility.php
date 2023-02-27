3927d44d4914954b765a853aa51eb75768adf1f3
Sainan <63328889+Sainan@users.noreply.github.com>
2023-02-27 14:13:07 +0100

    Add utility page

---
+++ utility.php
@@ -0,0 +1,10 @@
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://slv.do/base.css">
+<link rel="stylesheet" href="https://slv.do/navbar.css">
+<body>
+	<?php require "components/navbar.html"; ?>
+	<div>
+		<h1>Utility</h1>
+		<p>There is the .slv-hidden utility class, but it's kinda hard to <i>show</i>.<span class="slv-hidden"> See what I mean?</span></p>
+	</div>
+</body>
